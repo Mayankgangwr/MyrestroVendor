46d6fa5d02b89e86696e29143568703f384ae363
Mayank Gangwar <iammayankgangwarbly@gmail.com>
2023-01-14 13:07:06 +0000

    Update home.js and login.js

--- src/home.js
+++ src/home.js
@@ -1,21 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from "react-router-dom";
 const Home = () => {
-     return (
+    const [auth, setAuth] = useState("");
+    const navigate = useNavigate();
+    useEffect(()=>{
+        var emaildata = localStorage.getItem('email');
+        setAuth(emaildata); 
+        if(localStorage.getItem('email')===null){
+            navigate('/');
+        }  
+    }); 
+    const logout = () => {
+        localStorage.removeItem('email');
+        navigate('/');
+    }
+    return (
         <>
         <main style={{marginTop: '58px'}}>
         <div className="container pt-4">
-        <ul>
-        {groups[page].map((el,i)=>(
-            <li>{i+1}</li>
-        ))}    
-        </ul>
-        {groups.map((el,i)=>(
-            <button onClick={()=>setPage(i)}>{i+1}</button>
-        ))}    
+        <h1>Wellcome <span style={{color:'red'}}>{auth}</span></h1>
+        <button onClick={logout}>Logout</button>
         </div>
         </main>
         </>
     );
-  };
+  };""
   
   export default Home;
--- src/login.js
+++ src/login.js
@@ -3,28 +3,27 @@ import axios from "axios";
 import { useSelector, useDispatch } from 'react-redux';
 import { Login } from './action/index';
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 const Loginform = () =>{  
   const formdata = useSelector((state) => state.formData);
+  const dispatch = useDispatch();
   const [data, setData] = useState({
     name: "Prince Kurmi",
     password:"hello"
   });
-  const navigate  = useNavigate();
-  useEffect(() => {
-    localStorage.getItem('Name');
-    getUsers();
-},[]);
-function getUsers() {
-  const length = formdata.length;
-     console.log(formdata.length);
-   }
-  const dispatch = useDispatch();
-  console.log(formdata.length);
+  const  navigate  = useNavigate();
+  const Login = () =>{
+    axios
+      .post("https://sattasafari.com/restro/login.php", data)
+      .then(function (response) {
+        console.log(response.data);
+      });
+  }
  return(
         <>
           <main style={{marginTop: '58px'}}>
         <div className="container pt-4">
-        <button onClick={()=>dispatch(Login(data))}>Click</button>
+        <button onClick={Login}>Click</button>
         </div>
         </main>
         </>
