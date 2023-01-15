54bb20c77a0cc348b2bd080887d8c05179cea9ac
Mayank Gangwar <iammayankgangwarbly@gmail.com>
2023-01-15 15:53:12 +0000

    Update 3 files

--- src/products.js
+++ src/products.js
@@ -30,25 +30,39 @@ const Products = ()=>{
                 <li class="breadcrumb-item active" aria-current="page">Products</li>
             </ol>
         </nav>
+        <div className='pathhistory row my-2'>
+        <div className='col-6'>
+            <select className='form-control' name='category'>
+                <option value=''>Select Category</option>
+                <option value='1'>FastFood</option>
+            </select>
+        </div>
+        <div className='col-6'>
+            <input className='form-control' name='category' placeholder='Search by name' />
+        </div>
+        </div>
         <div className='blockborder row'>
         {orderdata.map((el)=>(
-                    <div key={el.id} className="col-xl-3 card-item col-lg-4 col-md-6 col-sm-6 col-xs-6 col-12 mt-3">
-                    <div className="card">
+                    <div key={el.id} className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-xs-6 col-12 mt-3">
+                    <div className="card card-item ">
                         <div className="card-body p-2">
                            <div className="d-flex text-black p-0">
                               <img src={el.img}
                                 alt="Generic placeholder image" className="item-logo" />    
                               <div className='row ps-3'> 
                                    <div className='col-12 d-flex justify-content-between'>
-                                        <h6 className='text-start'>{el.title}</h6>
-                                        <i class="fas fa-trash pro-icon mt-1 me-1"></i>
+                                        <div>
+                                        <h6 className='text-start mb-0'>{el.title}</h6>
+                                        <span>FastFood</span>
+                                        </div>
+                                        <i class="fas fa-trash pro-icon mt-1 me-1" style={{cursor:'pointer'}}></i>
                                    </div>
                                    <div className='col-12 d-flex justify-content-between'>
                                       <div>
                                       <span className='price sale-price pe-2'>${el.price}</span>
                                       <span className='price mrp'>${el.mrp}</span>
                                       </div>
-                                      <i class="fas fa-edit pro-icon mt-1 me-1"></i>
+                                      <i class="fas fa-edit pro-icon mt-1 me-1" style={{cursor:'pointer'}}></i>
                                    </div>
                               </div>
                            </div>
