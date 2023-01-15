96720a6c28a998d171d61dd4e8fd3da802729c0b
Mayank Gangwar <iammayankgangwarbly@gmail.com>
2023-01-15 16:45:42 +0000

    Update products.js

--- src/products.js
+++ src/products.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React,{useState, useEffect} from 'react';
 import {ReadOrder} from './action/index';
 import axios from 'axios';
 import './product.css';
@@ -6,7 +6,20 @@ import { useSelector, useDispatch } from 'react-redux';
 const Products = ()=>{
     const orderdata = useSelector((state) => state.OrderData);
     const dispatch = useDispatch();
+    const [category, setCategory] = useState([]);
+    const [filter, setFilter] = useState({
+        category:"",
+        proname:""
+    });
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setFilter({...filter, [name]:value});
+    }
     useEffect(() => {
+        getPro()
+    }, []);
+    function getPro(){
         const data = {
             restroid:localStorage.getItem('restroid')
         }
@@ -15,7 +28,10 @@ const Products = ()=>{
            console.log(response.data);
            dispatch(ReadOrder(response.data));
         });
-    }, []);
+    }
+    const prodata = orderdata.filter((item)=>{
+            return item.cat_id==filter.category;
+    });
     return (
         <>
         <main style={{marginTop: '58px'}}>
@@ -30,19 +46,19 @@ const Products = ()=>{
                 <li class="breadcrumb-item active" aria-current="page">Products</li>
             </ol>
         </nav>
-        <div className='pathhistory row my-2'>
+        <div className='blockborder row py-2'>
         <div className='col-6'>
-            <select className='form-control' name='category'>
+            <select className='form-control' style={{borderRadius:'10px'}} onChange={handleChange} name='category'>
                 <option value=''>Select Category</option>
                 <option value='1'>FastFood</option>
             </select>
         </div>
         <div className='col-6'>
-            <input className='form-control' name='category' placeholder='Search by name' />
+            <input className='form-control' value={filter.proname} onChange={handleChange} style={{borderRadius:'10px'}} name='proname' placeholder='Search by name' />
         </div>
         </div>
         <div className='blockborder row'>
-        {orderdata.map((el)=>(
+        {prodata.map((el)=>(
                     <div key={el.id} className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-xs-6 col-12 mt-3">
                     <div className="card card-item ">
                         <div className="card-body p-2">
@@ -53,7 +69,7 @@ const Products = ()=>{
                                    <div className='col-12 d-flex justify-content-between'>
                                         <div>
                                         <h6 className='text-start mb-0'>{el.title}</h6>
-                                        <span>FastFood</span>
+                                        <span>{el.cat_name}</span>
                                         </div>
                                         <i class="fas fa-trash pro-icon mt-1 me-1" style={{cursor:'pointer'}}></i>
                                    </div>
